bd9d096eec4d94a1dcffb58dcd69eeb46893efb5
Jinx0110 <janleecols@gmail.com>
2025-10-04 06:19:11 +0200

    insta feed still needs work

---
+++ TRAFFIK_API/Controllers/InstagramPostConstroller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TRAFFIK_API.Models;
+
+namespace TRAFFIK_API.Controllers
+{
+    [Route("api/InstagramPost")]
+    [ApiController]
+    public class InstagramFeedController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public InstagramFeedController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<InstagramPost>>> GetInstagramPosts()
+        {
+            var accessToken = "";
+            var url = $"https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp&access_token={accessToken}";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                    return StatusCode((int)response.StatusCode, "Failed to fetch Instagram posts");
+
+                var json = await response.Content.ReadAsStringAsync();
+                var posts = JsonSerializer.Deserialize<List<InstagramPost>>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return Ok(posts);
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while retrieving Instagram posts.");
+            }
+        }
+    }
+}
---
+++ TRAFFIK_API/Models/InstagramPost.cs
@@ -0,0 +1,12 @@
+﻿namespace TRAFFIK_API.Models
+{
+    public class InstagramPost
+    {
+        public string Id { get; set; }
+        public string Caption { get; set; }
+        public string MediaUrl { get; set; }
+        public string MediaType { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
